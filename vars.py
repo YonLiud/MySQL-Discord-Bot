e2d5da21ac2fd6b2aa1d525099a10ccfc329ec9d
Yonatan Mark Liudmirsky <45634836+YonLiud@users.noreply.github.com>
2020-10-05 18:28:18 +0300

    upload vars.py

---
+++ vars.py
@@ -0,0 +1,24 @@
+import os
+
+
+#! ▓▓                ░░
+#! ▓▓                ████              ██████▒▒      ████░░    ██    ██░░██████████░░██    ▒▒██████░░    ██▒▒    ██
+#! ▓▓              ▒▒    ░░          ██▒▒    ██░░  ████████    ██    ██░░    ██    ░░██  ▓▓██░░  ▒▒██░░  ████░░  ██
+#! ▓▓              ██  ░░██        ░░██            ████  ██    ██    ██░░    ██    ░░██  ██▓▓      ██▓▓  ██▓▓██  ██
+#! ▓▓            ▒▒██  ░░██        ░░██          ▒▒██░░  ██░░  ██    ██▒▒    ██    ░░██░░██▓▓      ████  ██  ██░░██
+#! ▓▓            ████    ██▓▓        ██      ██░░████████████  ██    ██░░    ██    ░░██  ████      ██▒▒  ██    ████
+#! ▓▓          ▒▒████▒▒▓▓████        ▓▓██▓▓████  ██      ░░██░░██▓▓▓▓██      ██    ░░██  ░░████▒▒████    ██    ▓▓██
+#! ▓▓          ██████  ▒▒██████        ░░░░                ░░                ░░      ░░        ▓▓        ░░      ░░
+#! ▓▓          ░░░░░░░░░░░░░░░░
+
+#! DO NOT RENAME THIS FILE
+
+
+WHITELIST_IDS = [477870815581569034, 271714102647324676, 347668690973753345] #? Place wanted whitelisted IDs in here as shown here
+
+TOKEN = 'TOKEN HERE' #? Place token inside ' '
+SYNTAX ='%>' #? Place wanted syntax/prefix inside ' '
+HOSTNAME = "127.0.0.1" #? Your's server IP
+USERNAME = "remoteuser" #? Remote user for the database
+PASSWORD = "Pa$$sw0rd" #? Password for database user
+DATABASE = "mysql" #? Database Name
