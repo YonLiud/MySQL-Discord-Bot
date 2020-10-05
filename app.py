5963640a4610868045beb4c2dcc198cbf05d46a1
YonLiud <ejliud@gmail.com>
2020-10-05 18:01:45 +0300

    Added main execution function

--- app.py
+++ app.py
@@ -10,6 +10,7 @@
 #! 888    Y888  "Y88888P"     888     888    888 8888888 888    Y888  "Y8888P88      888    888 8888888888 888   T88b 8888888888
 
 
+from re import sub
 from columnar import columnar
 import os
 import mysql.connector
@@ -39,15 +40,48 @@ except Exception as e:
     print("Exitting...")
     exit()
 
+def exec(query):
+    try:
+        cursor.execute(query)
+        return cursor.fetchall()
+    except Exception as e:
+        print(e)
+        return "ERR"
 @client.event
 async def on_message(message):
-    pass
+    data = []
+    query = ""
+    if message.author == client.user:
+        return
+    if message.content.startswith(syntax):
+        if message.content==syntax+"help":
+            await message.channel.send(embed=discord.Embed(title=client.user.name, description='https://devhints.io/mysql', color=discord.Color.purple()))
+            return
+        for word in message.content.split():
+            query += word + " "
+        query = query.replace(syntax, '')
+        if(query == " "):
+            await message.channel.send(embed=discord.Embed(title=client.user.name, description='Arguments are missing for the query', color=discord.Color.red()))
+            return
+        output = exec(query)
+        if output == "ERR":
+            await message.channel.send(embed=discord.Embed(title="An error occurred", description='See console for more information', color=discord.Color.red()))
+            return
+        for result in output:
+            sub_data = []
+            for x in result:
+                sub_data.append(x)
+            data.append(sub_data)
+        await message.channel.send(columnar(data, no_borders=False))
+
 @client.event
 async def on_ready():
-    print(f"client | Status:   Operational")
-    print(f"client | ID:       {format(client.user.id)}")
-    print(f"client | Name:     {format(client.user.name)}")
-    print(f"client | Guilds:   {len(client.guilds)}")
+    print(f"bot | Status:   Operational")
+    print(f"bot | ID:       {format(client.user.id)}")
+    print(f"bot | Name:     {format(client.user.name)}")
+    print(f"bot | Guilds:   {len(client.guilds)}")
+    print(f"bot Configurations: {config}")
+    print(f"Bot is ready to use")
     #? Custom Activity
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Syntax = " + syntax))
 
